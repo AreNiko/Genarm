abfacb486ad0a6b866624ec872de0fb1410ffe7d
Nikolai <nikolai.niko3@gmail.com>
2022-01-20 11:22:33 +0100

    Removed the bending check on gradient optimization models

--- python_model/run_rl.py
+++ python_model/run_rl.py
@@ -655,12 +655,15 @@ def runstuff(train_dir, test_number, use_pre_struct=True, continue_train=True, s
 			out = pi.numpy()
 			out[out <= 0.1] = 0
 			out[out > 0.1] = 1
+			"""
 			if np.sum(out) != 0:
 				try:
 					new_maxbend = eng.check_max_bend(convert_to_matlabint8(out[0]), vGextC, vGextF, nargout=1)
 					print("New vs old bending: ", new_maxbend, "/", og_maxbending)
 				except:
 					print("This one is singular :P")
+			"""
+			
 			#if np.sum(out) != 0 and show:
 			#	eng.clf(nargout=0)
 			#	eng.plotVg_safe(convert_to_matlabint8(out[0]), 'edgeOff', 'col',collist, nargout=0)
