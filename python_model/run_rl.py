46f55a85fe111494447d565ac6afaa027121e123
Nikolai <nikolai.niko3@gmail.com>
2022-02-01 13:36:39 +0100

    changed to only conv3D

--- python_model/run_rl.py
+++ python_model/run_rl.py
@@ -521,8 +521,7 @@ def runstuff(train_dir, test_number, use_pre_struct=True, continue_train=True, s
 	#os.makedirs(os.path.dirname("training_reinforcement/" + test_number + "/"), exist_ok=True)
 	checkpoint_path = base_dir + "/checkpoints/"
 	#checkpoint_path = os.path.dirname(checkpoint_path)
-	sess = tf.Session(config=tf.ConfigProto(
-  					intra_op_parallelism_threads=NUM_THREADS))
+	print(tf.config.threading.get_intra_op_parallelism_threads())
 	ckpt = tf.train.Checkpoint(
 		policy_network=policy_network,
 		value_network=value_network,
