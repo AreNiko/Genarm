437b677ad7a18dae8c5851a0db28b12c9f104328
Nikolai <nikolai.niko3@gmail.com>
2022-02-10 13:52:52 +0100

    Trying with other structures to test robustness

--- Functions/get_struct5.m
+++ Functions/get_struct5.m
@@ -1,34 +1,33 @@
 function [vG, vGextC, vGextF, vGstayOff] = get_struct5()
 
-    voxSize = 1; % 1 mm på printern, kan forandres for bedre eller dårligere vokseloppløsning
+    voxSize = 2; % 1 mm på printern, kan forandres for bedre eller dårligere vokseloppløsning
     xDist1 = round(20/voxSize); % Avstand fra venstre vegg til rød aksel. Med voxSize = 1, blir det 20stk voksler 
-    xDist2 = round(xDist1 + 140/voxSize); % Avstand fra rød aksel til første festeaksel
+    xDist2 = round(xDist1 + 40/voxSize); % Avstand fra rød aksel til første festeaksel
     xDist3 = round(xDist2 + 40/voxSize); % Avstand fra første til siste festeaksel
     radiusAxel = round(6/voxSize); % Med voxSize = 1, blir det 6stk voksler
     %%%%%%%%%%% Lager initiell vG
-    vG = zeros(xDist1*2+xDist3, radiusAxel*20, 10, 'int8');
+    vG = zeros(xDist1*2+xDist3, radiusAxel*20, 7, 'int8');
     yCenter = round(size(vG,2)/2);
-    vG = vGbox(vG,[3 round(yCenter-radiusAxel*5/2) 2],1, size(vG,1)-4,radiusAxel*5,10);
+    vG = vGbox(vG,[3 round(yCenter-radiusAxel*5/2) 2],1, size(vG,1)-4,radiusAxel*5,5);
     %%%%%%%%%%% Definerer låste voksler - blå
     vGextC = zeros(size(vG),'int8');
-    vGextC = vGcylinder(vGextC,[xDist2,yCenter,2],1, 2,10);
-    vGextC = vGcylinder(vGextC,[xDist3,yCenter,2],1, 2,10);
+    vGextC = vGcylinder(vGextC,[xDist2,yCenter,2],1, 2,5);
+    vGextC = vGcylinder(vGextC,[xDist3,yCenter,2],1, 2,5);
     %%%%%%%%%%% Definerer kraft voksler - grå
     vGextF = zeros(size(vG),'int8');
-    vGextF = vGcylinder(vGextF,[xDist1,yCenter,2],1, radiusAxel,10);
+    vGextF = vGcylinder(vGextF,[xDist1,yCenter,2],1, radiusAxel,5);
     %%%%%%%%%%% Definerer stayOff voksler - gule
     vGstayOff = zeros(size(vG),'int8');
-    vGstayOff = vGcylinder(vGstayOff,[xDist1,yCenter,2],1, radiusAxel+3,10);
-    vGstayOff = vGcylinder(vGstayOff,[xDist2,yCenter,2],1, radiusAxel+3,10);
-    vGstayOff = vGcylinder(vGstayOff,[xDist3,yCenter,2],1, radiusAxel+3,10);
-    %{
+    vGstayOff = vGcylinder(vGstayOff,[xDist1,yCenter,2],1, radiusAxel+3,5);
+    vGstayOff = vGcylinder(vGstayOff,[xDist2,yCenter,2],1, radiusAxel+3,5);
+    vGstayOff = vGcylinder(vGstayOff,[xDist3,yCenter,2],1, radiusAxel+3,5);
     figure(1);clf;plotVg(vG,'edgeOff');
     hold on;plotVg(vGstayOff,'edgeOff','col',[0.9 0.9 0.5]);
     hold on;plotVg(vGextF,'edgeOff','col',[0.5 0.5 0.5]);
     hold on;plotVg(vGextC,'edgeOff','col',[0.6 0.6 0.8]);
     tittle("Stay off (gul), extF (grå), låst (blå)");
     sun1;
-    
+    %{
     max_bend = check_max_bend(vG, vGextC, vGextF);
     disp(max_bend);
     
