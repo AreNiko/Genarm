d148e40f134827316f5c86be03c6e56e84127f0a
Nikolai <nikolai.niko3@gmail.com>
2022-02-14 18:23:14 +0100

    Added a try function for checking the bending

--- python_model/run_rl2.py
+++ python_model/run_rl2.py
@@ -333,9 +333,9 @@ def entropy_loss(pi):
 def flip_coord(pi_old, struct):
 	new_struct = struct.numpy()
 	batch, xdim, ydim, zdim = tf.shape(struct)
-	x = np.floor(pi_old[:,0]*tf.cast(xdim,tf.float32),dtype=np.unit)
-	y = np.floor(pi_old[:,1]*tf.cast(ydim,tf.float32),dtype=np.unit)
-	z = np.floor(pi_old[:,2]*tf.cast(zdim,tf.float32),dtype=np.unit)
+	x = np.floor(pi_old[:,0]*tf.cast(xdim,tf.float32),dtype=int8)
+	y = np.floor(pi_old[:,1]*tf.cast(ydim,tf.float32),dtype=int8)
+	z = np.floor(pi_old[:,2]*tf.cast(zdim,tf.float32),dtype=int8)
 	#x = tf.cast(x,tf.int32)
 	#y = tf.cast(y,tf.int32)
 	#z = tf.cast(z,tf.int32)
