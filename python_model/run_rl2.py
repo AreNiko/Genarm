e1958d9c26e8dcc88459f0d41db1d505289451bb
Nikolai <nikolai.niko3@gmail.com>
2022-02-16 13:58:21 +0100

    Added a try function for checking the bending

--- python_model/run_rl2.py
+++ python_model/run_rl2.py
@@ -390,9 +390,7 @@ def sample_episodes(obser, policy_network, num_episodes, maxlen, action_repeat=1
 			action = tf.random.categorical(logits, 30)[0]
 			#action = tf.math.sigmoid(tf.cast(action,tf.float32))
 			action = tf.cast(tf.reshape(action, [10,3]),tf.float32)
-			print(action)
-			action = action/tf.math.reduce_max(action)
-			print(action)
+			action = action/150
 			pi_old = activations.softmax(logits)[0]
 			
 			episode.observations.append(observation[0])
