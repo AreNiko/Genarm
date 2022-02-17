04aa61d7eb7492c8bcae18e625ab00fa2d57a892
Nikolai <nikolai.niko3@gmail.com>
2022-02-17 15:01:00 +0100

    Added a try function for checking the bending

--- python_model/run_rl2.py
+++ python_model/run_rl2.py
@@ -396,7 +396,7 @@ def sample_episodes(obser, policy_network, num_episodes, maxlen, action_repeat=1
 			#action = tf.random.categorical(logits, 150)[0]
 			#action = tf.math.sigmoid(tf.cast(action,tf.float32))
 			action = tf.reshape(logits[0], [50,3])
-			noise = tf.random.normal(shape = tf.shape(action), mean = 0.0, stddev = 0.1, dtype = tf.float32)
+			noise = tf.random.normal(shape = tf.shape(action), mean = 0.0, stddev = 0.05, dtype = tf.float32)
 			action = action + noise
 			action = tf.clip_by_value(action, 0, 1)
 			#action = action/150
