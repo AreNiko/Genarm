680284a7243289e4cf770c2a2ce9df3ffeabe091
Nikolai <nikolai.niko3@gmail.com>
2022-02-20 14:09:48 +0100

    Added a try function for checking the bending

--- python_model/run_rl2.py
+++ python_model/run_rl2.py
@@ -547,7 +547,7 @@ def runstuff(train_dir, test_number, use_pre_struct=True, continue_train=True, s
 	action_repeat = 1
 	maxlen = maxlen_environment // action_repeat # max number of actions
 	batch_size = 1
-	checkpoint_interval = 5
+	checkpoint_interval = 50
 	eval_interval = 1
 	eval_episodes = 1
 
@@ -734,25 +734,13 @@ def runstuff(train_dir, test_number, use_pre_struct=True, continue_train=True, s
 				with tf.GradientTape() as tape:
 					pi = activations.softmax(policy_network.policy(obs))
 					v = value_network(obs, np.float32(maxlen)-t)
-					
-					#p_loss = policy_loss(pi, pi_old, advantage, epsilon)
-					#v_loss = c1*value_loss(value_target, v)
-					#e_loss = c2*entropy_loss(pi)
+
+					pi_a = tf.squeeze(tf.gather(pi, action, batch_dims=1), -1)
+                    pi_old_a = tf.squeeze(tf.gather(pi_old, action, batch_dims=1), -1)
+
 					loss = policy_loss(pi, pi_old, advantage, epsilon) \
 						 + c1*value_loss(value_target, v) \
 						 + c2*entropy_loss(pi)
-				#print(p_loss, v_loss, e_loss)
-				#loss = p_loss + v_loss + e_loss
-				#print(tf.shape(pi))
-				#print(tf.shape(action))
-				#pi_a = tf.stack([pi, action], axis=1)[0]
-				#pi_old_a = tf.stack([pi_old, action], axis=1)[0]
-				#pi_a = tf.gather(pi, tf.cast(action, tf.int32))[0]
-				#pi_old_a = tf.gather(pi_old, tf.cast(action, tf.int32))[0]
-				#print(tf.shape(pi_a))
-				#print(pi)
-				#print(pi_a)
-				#print(pi_old_a)
 
 				trainable_variables = policy_network.trainable_variables + value_network.trainable_variables
 				# Get unique list of variables, just adding lists may cause issues if shared variables
