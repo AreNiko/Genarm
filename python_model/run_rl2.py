fa08b8636e7643a9d7aa2dd426ab69a4db07c563
Nikolai <nikolai.niko3@gmail.com>
2022-02-25 16:23:26 +0100

    Added a try function for checking the bending

--- python_model/run_rl2.py
+++ python_model/run_rl2.py
@@ -109,7 +109,7 @@ def eval_policy(obser, agent, maxlen_environment, eval_episodes, action_repeat):
 						#print(new_bend, vox_diff, comps)
 						
 						#reward = bend_diff + place_diff/5 - (vox_diff/10 + (comps-1))
-						reward = (bend_diff - 1) - (vox_diff + (comps-1))/10
+						reward = 10*(bend_diff - 1) - (vox_diff + (comps-1))/100
 						print('| {:14s} | {:14f} |'.format('Old bending:', og_bend))
 						print('| {:14s} | {:14f} |'.format('New bending:', new_bend))
 						print('| {:14s} | {:14d} |'.format('Voxels diff:', int(vox_diff)))
