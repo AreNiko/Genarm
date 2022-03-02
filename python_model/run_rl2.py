e2284f98031409ba39315cb0b595fb8a31c839e3
Nikolai <nikolai.niko3@gmail.com>
2022-03-02 11:17:39 +0100

    Added a try function for checking the bending

--- python_model/run_rl2.py
+++ python_model/run_rl2.py
@@ -107,7 +107,7 @@ def eval_policy(obser, agent, maxlen_environment, eval_episodes, action_repeat):
 						#r = 10*(bend_diff - 1) - (vox_diff + (comps-1))/100
 						#r = 2*(bend_diff - 1)
 						#reward = 1000*(og_bend - new_bend) - (vox_diff + 5*(comps-1))/100
-						reward = 1000*(og_bend - new_bend) + (32 - (comps-1))/1000
+						reward = 1000*(og_bend - new_bend) + (1 - (comps-1))/10
 						print('| {:14s} | {:14f} |'.format('Old bending:', og_bend))
 						print('| {:14s} | {:14f} |'.format('New bending:', new_bend))
 						print('| {:14s} | {:14d} |'.format('Voxels diff:', int(vox_diff)))
@@ -474,7 +474,7 @@ def sample_episodes(obser, policy_network, num_episodes, maxlen, action_repeat=1
 						#r = 10*(bend_diff - 1) - (vox_diff + (comps-1))/100
 						#r = 2*(bend_diff - 1)
 						#r = 1000*(og_bend - new_bend) - (vox_diff + 5*(comps-1))/100
-						r = 1000*(og_bend - new_bend) + (32 - (comps-1))/1000
+						r = 1000*(og_bend - new_bend) + (1 - (comps-1))/10
 						print('| {:14s} | {:14f} |'.format('Old bending:', og_bend))
 						print('| {:14s} | {:14f} |'.format('New bending:', new_bend))
 						print('| {:14s} | {:14d} |'.format('Voxels diff:', int(vox_diff)))
