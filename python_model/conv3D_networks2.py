992ec0838c6bfe2204d7ea774f61f90b2ca8c93d
Are Nikolai Sigersvold <arensi@dancer.ifi.uio.no>
2022-05-13 17:11:33 +0200

    Added data from the reinforcement learning experiments

--- python_model/conv3D_networks2.py
+++ python_model/conv3D_networks2.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import activations, layers, optimizers
-
+import numpy as np
 
 
 class FeatureExtractor(tf.keras.Model):
@@ -8,10 +8,11 @@ class FeatureExtractor(tf.keras.Model):
     def __init__(self, **kwargs):
         super(FeatureExtractor, self).__init__(**kwargs)
 
-        self.conv3d1 = layers.Conv3D(16, 5, strides=1, padding='same', activation='relu')
-        self.conv3d2 = layers.Conv3D(16, 5, strides=1, padding='same', activation='relu')
-        self.conv3d3 = layers.Conv3D(16, 5, strides=1, padding='same', activation='relu')
-        self.conv3d4 = layers.Conv3D(16, 5, strides=1, padding='same', activation='relu')
+        self.conv3d1 = layers.Conv3D(128, 3, strides=1, padding='same', activation='relu')
+        self.conv3d2 = layers.Conv3D(64, 3, strides=1, padding='same', activation='relu')
+        self.conv3d3 = layers.Conv3D(32, 3, strides=1, padding='same', activation='relu')
+        self.conv3d4 = layers.Conv3D(16, 3, strides=1, padding='same', activation='relu')
+        self.conv3d5 = layers.Conv3D(8, 3, strides=1, padding='same', activation='relu')
         self.conv1x1 = layers.Conv3D(1, 1, strides=(1, 1, 1), padding='same')
         self.batchmeup = layers.BatchNormalization(momentum=0.8)
         self.flatten = layers.Flatten()
@@ -35,29 +36,11 @@ class FeatureExtractor(tf.keras.Model):
         #struct = layers.Input(input_shape)
 
         x1 = self.conv3d1(input_shape)
-        #x1 = self.conv1x1(x1)
-        #x1 = self.conv3d1(x1)
-        #x1 = self.batchmeup(x1)
-        
         x2 = self.conv3d2(x1)
-        #x2 = self.conv1x1(x2)
-        #x2 = self.conv3d2(x2)
-        #x2 = self.batchmeup(x2)
-        
         x3 = self.conv3d3(x2)
-        #x3 = self.conv1x1(x3)
-        #x3 = self.conv3d3(x3)
-        #x3 = self.batchmeup(x3)
-        
         x4 = self.conv3d4(x3)
-        #x4 = self.conv1x1(x4)
-        #x4 = self.batchmeup(x4)
-        
-        #x1 = self.flatten(x1)
-        #x2 = self.flatten(x2)
-        #x3 = self.flatten(x3)
-        #x4 = self.flatten(x4)
-        xf = tf.concat([x1, x2, x3, x4], -1)
+        x5 = self.conv3d5(x4)
+        xf = tf.concat([x1, x2, x3, x4, x5], -1)
         #xf = tf.squeeze(xf)
         #model = models.Model(inputs=[struct, locked, forces, stayoff], outputs=xfd)
         #return x1,x2,x3,x4
@@ -70,13 +53,13 @@ class PolicyNetwork(tf.keras.Model):
 
     def __init__(self, feature_extractor, **kwargs):
         super(PolicyNetwork, self).__init__(**kwargs)
-        
+
         self.feature_extractor = feature_extractor
-        self.conv3d1 = layers.Conv3D(16, 5, strides=(1, 1, 1), padding='same', activation='relu')
-        self.conv3d2 = layers.Conv3D(32, 5, strides=(1, 1, 1), padding='same', activation='relu')
-        self.conv3d3 = layers.Conv3D(64, 5, strides=(1, 1, 1), padding='same', activation='relu')
-        self.conv3d4 = layers.Conv3D(32, 5, strides=(1, 1, 1), padding='same', activation='relu')
-        self.conv3d5 = layers.Conv3D(16, 5, strides=(1, 1, 1), padding='same', activation='relu')
+        self.conv3d1 = layers.Conv3D(16, 3, strides=(1, 1, 1), padding='same', activation='relu')
+        self.conv3d2 = layers.Conv3D(32, 3, strides=(1, 1, 1), padding='same', activation='relu')
+        self.conv3d3 = layers.Conv3D(64, 3, strides=(1, 1, 1), padding='same', activation='relu')
+        self.conv3d4 = layers.Conv3D(32, 3, strides=(1, 1, 1), padding='same', activation='relu')
+        self.conv3d5 = layers.Conv3D(16, 3, strides=(1, 1, 1), padding='same', activation='relu')
         """
         self.dense512_1 = layers.Dense(512, activation='relu')
         self.dense512_2 = layers.Dense(512, activation='relu')
@@ -128,13 +111,13 @@ class PolicyNetwork(tf.keras.Model):
 
         #xf = tf.concat([x1d, x2d], -1)
         #xf = tf.concat([x1d, x2d, x3d, x4d], -1)
-        
+
         #xf = self.dense128(xf)
         #xf = self.dense128(xf)
         #xf = self.dense512(xf)
         #xf = self.dense512(xf)
         #xfd = layers.Conv3D(1, 1, strides=(1, 1, 1), padding='same', activation='relu')(xf)
-        
+
         #xfd = tf.keras.activations.tanh(xfd)
         #xfd = tf.keras.activations.sigmoid(xfd)
         #xfd = self.dense_coord(x5d)
@@ -160,7 +143,7 @@ class PolicyNetwork(tf.keras.Model):
 
     def call(self, x):
 
-        logits = self.policy(x)
+        logits = activations.softmax(self.policy(x))
         action = self._sample_action(logits)
 
         return action
